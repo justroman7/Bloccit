15a3cf3ba784f92a23eed162b8f25080309baa9a
Justin Romanowski <just.roman7@gmail.com>
2017-12-31 12:39:58 -0700

    finished assingment 16 part 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,6 @@ posts = Post.all
   )
 end
 
-Post.find_or_create_by(title: "Unique title", body: "unique body")
-
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
