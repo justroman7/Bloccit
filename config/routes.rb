fa40d3a656ad3a6ed53656340ce434ed84161420
Justin Romanowski <just.roman7@gmail.com>
2018-01-01 16:02:23 -0700

    finished checkpoint-17

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
-
-  get 'welcome/contact'
-
-  get 'welcome/faq'
-
+  resources :posts
+  get 'about' => 'welcome#about'
   root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  end
