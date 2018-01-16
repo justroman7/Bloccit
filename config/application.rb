9bcfc7609a538e05d6bab7ee38a91bb1c7655c36
Justin <just.roman7@gmail.com>
2018-01-16 13:45:20 -0700

    finished assignment-24

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module Bloccit
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.autoload_paths << File.join(config.root, "lib")
+    config.action_controller.permit_all_parameters = true
   end
 end
